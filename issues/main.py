fc4d85169cc6870dc724e51401e5c225c8bcd352
YuChen Ding <201250012@smail.nju.edu.cn>
2023-05-10 21:09:01 +0800

    feat: download github issue comments as well

--- issues/main.py
+++ issues/main.py
@@ -4,58 +4,94 @@ import os
 import random
 import time
 from concurrent.futures import ThreadPoolExecutor
+from enum import Enum
 from queue import Queue
 
 import requests
 
+
+class DataType(Enum):
+    ISSUE_TITLE = 1,
+    ISSUE_COMMENT = 2
+
+
 OWNER = "microsoft"
 REPO = "vscode"
-PAGES = 1
+PAGE_ST = 46
+PAGE_ED = 46
 PER_PAGE = 100
-limited = False
-
-fieldnames = ['id', 'text']
+TYPE = DataType.ISSUE_COMMENT
 
 
-# 定义一个下载 issue 数据的函数
-def download_issues(owner, repo, page):
+def clean(text: str) -> str:
+    return text.strip().replace('\n', ' ')
+
+
+def get_proxy():
+    return requests.get("http://demo.spiderpy.cn/get/").json()
+
+
+# 下载函数
+def downloader(owner, repo, page):
     headers = {
         "Accept": "application/vnd.github+json",
         "X-GitHub-Api-Version": "2022-11-28"
     }
+    params = {
+        "sort": "created",
+        "direction": "asc",
+        "per_page": PER_PAGE,
+        "page": page
+    }
+
+    if TYPE == DataType.ISSUE_TITLE:
+        params.setdefault("state", "all")
+        url = "issues"
+        dict_key = "title"
+        filename = f"{owner}.{repo}.issues.{page}.csv"
+    elif TYPE == DataType.ISSUE_COMMENT:
+        url = "issues/comments"
+        filename = f"{owner}.{repo}.comments.{page}.csv"
+        dict_key = "body"
+    else:
+        raise RuntimeError("unsupported type")
+
+    proxy = get_proxy().get("proxy")
     response = requests.get(
-        f"https://api.github.com/repos/{owner}/{repo}/issues?state=open&per_page={PER_PAGE}&page={page + 1}",
-        headers=headers)
+        f"https://api.github.com/repos/{owner}/{repo}/{url}", params=params, headers=headers,
+        proxies={"http": "http://{}".format(proxy)})
     if response.status_code != requests.codes.ok:
-        global limited
-        limited = True
         print("rate limit")
-        return
+        raise RuntimeError("rate limit")
+
     raw = response.content.decode()
     issues = json.loads(raw)
-    issues = [{"id": PER_PAGE * page + idx, "text": issue["title"]}
-              for idx, issue in enumerate(issues)
-              if len(issue["title"]) > 32]
-    with open(f"{owner}.{repo}.{page}.csv", 'w', newline='') as f:
+    issues = [{"id": (page - 1) * PER_PAGE + idx + 1, "text": clean(issue[dict_key])} for idx, issue in
+              enumerate(issues)]
+
+    fieldnames = ['id', 'text']
+    with open(filename, 'w', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter='|')
-        writer.writeheader()
+        # writer.writeheader()
         writer.writerows(issues)
+
+    print(page)
     time.sleep(random.randint(3, 7))
 
 
-# 定义一个任务队列
+# 任务队列
 task_queue = Queue()
 
 # 将所有任务添加到队列中
-for i in range(PAGES):
+for i in range(PAGE_ST, PAGE_ED + 1):
     task_queue.put(i)
 
 # 创建线程池
 with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
     # 不断地从任务队列中取出任务并执行
-    while not task_queue.empty() and not limited:
+    while not task_queue.empty():
         i = task_queue.get()
-        executor.submit(download_issues, OWNER, REPO, i)
+        executor.submit(downloader, OWNER, REPO, i)
         task_queue.task_done()
 
-print("DONE")
+print("ALL DONE")
